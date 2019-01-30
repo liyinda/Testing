67ad5aabbc43357f88d791cff36def818a7247b8
root <liyinda0000@163.com>
2019-01-30 15:36:49 +0800

    update account.go

--- account.go
+++ account.go
@@ -4,6 +4,9 @@ import (
   "fmt"
   "bufio"
   "os"
+  "io"
+  "strings"
+  "strconv"
 )
 type Account struct {
   AccountNo string
@@ -59,6 +62,7 @@ func (account *Account) ChangePwd(pwd string, newpwd string) {
 
 }
 
+
 func main() {
   dadaAccount := &Account {
     AccountNo : "rm111111" ,
@@ -66,19 +70,69 @@ func main() {
     Balance : 100.0 ,
   }
   fmt.Println("1查询，2取款，3存款，4更改秘密")
-  reader := bufio.NewReader(os.Stdin)
-  button, _, _ := reader.ReadLine()
-
-  switch button {
-    case "1":
-      dadaAccount.Query("888888")
-    case "2":
-      dadaAccount.Withdraw(150, "888888")
-    case "3":
-      dadaAccount.Deposite(200, "888888")
-    case "4":
-      dadaAccount.ChangePwd("888888","123123")
+  buff := bufio.NewReader(os.Stdin)
+  for {
+    line, err := buff.ReadString('\n')
+    if err != nil || io.EOF == err {
+      break
+    }
+    switch line {
+    case "1\n":
+      fmt.Println("请输入查询密码")
+      input, err:= buff.ReadString('\n')
+      if err != nil {
+        return
+      }
+      input = strings.Replace(input,"\n","",-1)
+      dadaAccount.Query(input)
+    case "2\n":
+      fmt.Println("请输入账号密码")
+      inputpwd, err:= buff.ReadString('\n')
+      if err != nil {
+        return
+      }
+      inputpwd = strings.Replace(inputpwd,"\n","",-1)
+      fmt.Println("请输入取款金额")
+      inputwithdraw, err:= buff.ReadString('\n')
+      if err != nil {
+        return
+      }
+      inputwithdraw = strings.Replace(inputwithdraw,"\n","",-1)
+      inputfloat,err := strconv.ParseFloat(inputwithdraw,64)
+      dadaAccount.Withdraw(inputfloat, inputpwd)
+    case "3\n":
+      fmt.Println("请输入账号密码")
+      inputpwd, err:= buff.ReadString('\n')
+      if err != nil {
+        return
+      }
+      inputpwd = strings.Replace(inputpwd,"\n","",-1)
+      fmt.Println("请输入存款金额")
+      inputdeposite, err:= buff.ReadString('\n')
+      if err != nil {
+        return
+      }
+      inputdeposite = strings.Replace(inputdeposite,"\n","",-1)
+      inputfloat,err := strconv.ParseFloat(inputdeposite,64)
+      dadaAccount.Deposite(inputfloat, inputpwd)
+    case "4\n":
+      fmt.Println("请输入密码")
+      inputpwd, err:= buff.ReadString('\n')
+      if err != nil {
+        return
+      }
+      inputpwd = strings.Replace(inputpwd,"\n","",-1)
+      fmt.Println("请输入新密码")
+      inputnewpwd, err:= buff.ReadString('\n')
+      if err != nil {
+        return
+      }
+      inputnewpwd = strings.Replace(inputnewpwd,"\n","",-1)
+      dadaAccount.ChangePwd(inputpwd,inputnewpwd)
     default:
       fmt.Println("1查询，2取款，3存款，4更改密码")
+    }
   }
 }
+
+
